a906f453fd217cec88ef06d028ad583fad6ee8ba
juliajykim <jkim24@gwu.edu>
2021-05-04 15:24:52 -0400

    Finish Jbuilder project

---
+++ jbuilder/app/views/api/parties/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @parties, :id, :name, :location
--- jbuilder/config/routes.rb
+++ jbuilder/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :guests, only: [:show, :index] do
       resources :gifts, only: [:index]
     end 
+    resources :gifts, only: [:show]
     resources :parties, only: [:show, :index]
   end 
 
