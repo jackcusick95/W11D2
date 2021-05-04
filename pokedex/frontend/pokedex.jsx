9cd9a18728256a6ec2f0006eb45632a3c7a5cf7d
juliajykim <jkim24@gwu.edu>
2021-05-04 17:30:46 -0400

    Test store and reducer

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { receiveAllPokemon } from './actions/pokemon_actions'
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
     const rootEl = document.getElementById('root');
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
+    window.receiveAllPokemon = receiveAllPokemon;
     ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
